0a5d636c20893a2dabb457a50a05de699b8763b3
rodrigojarr <rodrigoclcunha@gmail.com>
2025-12-03 17:23:30 -0300

    Update MainActivity.kt

--- app/src/main/java/br/ufpr/trabmob2/MainActivity.kt
+++ app/src/main/java/br/ufpr/trabmob2/MainActivity.kt
@@ -12,11 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
+ 
         val btnListStudents = findViewById<Button>(R.id.btnListStudents)
         val btnListTeachers = findViewById<Button>(R.id.btnListTeacher)
         val btnFindCharacter= findViewById<Button>(R.id.btnListCharacterId)
